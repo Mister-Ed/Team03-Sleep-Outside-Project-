ccc3c0644004a5c3187480a6a10c9b84d567d79e
hvelarderiquelme <hvelardeab@gmail.com>
2026-01-09 11:34:02 -0700

    try 2

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
+  const cartItems = getLocalStorage("so-cart", ""); // get cart array of items from local storage if null set to empty array
+  //cartItems.push();
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
